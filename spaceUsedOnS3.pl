7f305c27637a892fe9ea1819ea696db4553686c8
philip r brenan <philiprbrenan@users.noreply.github.com>
2017-02-27 03:53:04 +0000

    Add files via upload

--- spaceUsedOnS3.pl
+++ spaceUsedOnS3.pl
@@ -8,9 +8,8 @@ require v5.16;
 use warnings FATAL => qw(all);
 use strict;
 use Data::Table::Text qw(:all);
-use Data::Dump qw(dump);
 
-my $buckets = qx(aws s3 ls  --recursive --human-readable --summarize);          # Buckets
+my $buckets = qx(aws s3 ls);                                                    # Buckets
 my @buckets = map {(split /\s+/, $_)[2]} split /\n/, $buckets;
 pop @buckets for 1..2;                                                          # Remove extraneous entries
 
@@ -20,7 +19,6 @@ my @results;
 
 for my $bucket(@buckets)                                                        # Each bucket
  {my $c = "aws s3 ls s3://$bucket --recursive --human-readable --summarize";
-# next unless $bucket =~ /phil|allan/i;
   my $r = qx($c);
   my ($objects)      = $r =~ m/Total Objects:\s+(\d+)/;
   my ($size, $scale) = $r =~ m/Total Size:\s([0-9.]+)\s+(\w+)/;
